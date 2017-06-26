d3eb3449e66bf4515a05ba9ef06a3cebad069fea
ohguchi.tsukasa <ohguchi.tsukasa@ohguchi>
2017-06-26 10:42:02 +0900

    k

---
+++ inventory-control/src/jp/co/comnic/javalesson/inventory/control/entity/Account.java
@@ -0,0 +1,56 @@
+package jp.co.comnic.javalesson.inventory.control.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ * The persistent class for the ACCOUNT database table.
+ * 
+ */
+
+@Entity
+public class Account implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+
+	private String password;
+
+	private String username;
+
+	public Account() {
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [email=" + email + "]";
+	}
+}
