6a4d9fd257e9f725aee195cdfec0a8d8f915b386
ogawa.keiichi <ogawa.keiichi@ogawa>
2017-06-26 10:18:40 +0900

    Signed-off-by: ogawa.keiichi <ogawa.keiichi@ogawa>

--- inventory-control/src/jp/co/comnic/javalesson/inventory/control/entity/Account.java
+++ inventory-control/src/jp/co/comnic/javalesson/inventory/control/entity/Account.java
@@ -54,33 +54,3 @@ public class Account implements Serializable {
 		return "Account [email=" + email + "]";
 	}
 }
-//public class Account implements Serializable {
-//	private static final long serialVersionUID = 1L;
-//
-//	
-//	private String email;
-//
-//	private String password;
-//
-//
-//	public Account() {
-//	}
-//
-//	public String getEmail() {
-//		return this.email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	public String getPassword() {
-//		return this.password;
-//	}
-//
-//	public void setPassword(String password) {
-//		this.password = password;
-//		
-//	}
-
-}
